341c5363e35e35b6c0c7cc272c4d8d63278a9433
miteshchopda <miteshchopda00@gmail.com>
2026-03-10 00:28:38 +0530

    add streaming

--- src/api_call.py
+++ src/api_call.py
@@ -1,4 +1,5 @@
 import requests
+import json
 from config import API_KEY_REF, MODEL_NAME
 from rich.console import Console
 from rich.markdown import Markdown
@@ -10,33 +11,49 @@ def main(prompt):
     url = "https://openrouter.ai/api/v1/chat/completions"
     headers = {
         "Authorization": f"Bearer {API_KEY_REF}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
 
     payload = {
         "model": f"{MODEL_NAME}",
         "messages": [
             {
-                "role": "user",
-                "content": f"{prompt}"
-            }
+                "role": "system",
+                "content": "You are a helpful assistant. Keep it short if possible.",
+            },
+            {"role": "user", "content": f"{prompt}"},
         ],
-        "modalities": ["text"]
+        "modalities": ["text"],
+        "stream": True,
     }
 
-    try:
-        response = requests.post(url, headers=headers, json=payload)
-        result = response.json()
+    response = requests.post(url=url, headers=headers, json=payload, stream=True)
 
-        for choice in result["choices"]:
-            content = choice["message"]["content"]
-            md = Markdown(content)
-            print("---RESPONSE---")
-            console.print(md)
-            # print(content)
-            print("---RESPONSE-END---")
-            with open("response.md",'a') as response_file:
-                response_file.write(content + "\n")
+    last_response_by = ""
+    for line in response.iter_lines():
+        if line:
+            line_str = line.decode("utf-8")
+            if line_str.startswith("data: "):
+                data = line_str[6:]
+                if data == "[DONE]":
+                    break
+                try:
+                    parsed = json.loads(data)
+                    # console.log(parsed)
+                    with open("logs.py", "a") as logs:
+                        logs.write(json.dumps(parsed) + "\n")
 
-    except Exception as e:
-        print(e)
+                    if parsed["choices"]:
+                        # I dont actually know what delta is but it contains content, role.
+                        delta = parsed["choices"][0]["delta"]
+
+                        content = delta["content"]
+                        role = delta["role"]
+                        last_response_by = role
+                        if content:
+                            if last_response_by == role:
+                                console.print(content, end="")
+                            else:
+                                console.print(f"[bold]{role}:[/bold] {content}")
+                except json.JSONDecodeError:
+                    continue
